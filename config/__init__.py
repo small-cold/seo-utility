ac9c276caafcd7855e90d6593b9361cfd8009b86
MicroCold <wlchao520@126.com>
2017-03-26 22:38:56 +0800

    添加配置文件

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from config import config
+__all__ = [
+    config.configs
+]
---
+++ config/config_default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Configuration
+'''
+import os
+
+__author__ = 'small cold'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '123456',
+        'db_name': 'data_analyze'
+    },
+}
